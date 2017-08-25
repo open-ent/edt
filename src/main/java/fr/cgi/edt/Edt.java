8263573705e6931a7ae64bf38e178d8f459da9c2
LEDUNOIS Simon <simon.ledunois@cgi.com>
2017-08-25 17:27:12 +0200

    Adding dependencies verifications based on core shared map. If one dependencies is wrong then mod is undeployed.

--- src/main/java/fr/cgi/edt/Edt.java
+++ src/main/java/fr/cgi/edt/Edt.java
@@ -5,6 +5,7 @@ import org.entcore.common.http.filter.ShareAndOwner;
 import org.entcore.common.mongodb.MongoDbConf;
 
 import fr.cgi.edt.controllers.EdtController;
+import org.vertx.java.core.Handler;
 import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.core.shareddata.ConcurrentSharedMap;
 
@@ -15,25 +16,46 @@ public class Edt extends BaseServer {
 	@Override
 	public void start() {
 		super.start();
+
 		addController(new EdtController(EDT_COLLECTION));
 		MongoDbConf.getInstance().setCollection(EDT_COLLECTION);
 		setDefaultResourceFilter(new ShareAndOwner());
-		checkDependencies();
+
+		vertx.setTimer(1000, new Handler<Long>() {
+			@Override
+			public void handle(Long aLong) {
+				if (!validDependencies()) {
+					ConcurrentSharedMap<String, String> deploymentsIdMap = vertx.sharedData().getMap("deploymentsId");
+					container.undeployModule(deploymentsIdMap.get("fr.cgi.edt"));
+				}
+			}
+		});
 	}
 
-	private void checkDependencies () {
-		JsonObject dependencies = config.getObject("dependencies");
-		final ConcurrentSharedMap<Object, Object> versionMap = vertx.sharedData().getMap("versions");
-		for (String mod: dependencies.getFieldNames()) {
-			if (!isMajor(versionMap.get(mod).toString(), dependencies.getString(mod))) {
-				log.error(mod + " minor version. Please upgrade " + mod + "version to " + dependencies.getString(mod) + " and upper");
+	private boolean validDependencies () {
+		Boolean isValid = true;
+		if ("prod".equals(config.getString("mode"))) {
+			JsonObject dependencies = config.getObject("dependencies");
+			final ConcurrentSharedMap<Object, Object> versionMap = vertx.sharedData().getMap("versions");
+			for (String mod : dependencies.getFieldNames()) {
+				if (!isMajor(versionMap.get(mod).toString(), dependencies.getString(mod))) {
+					log.error(mod + " minor version. Please upgrade " + mod + " version to " + dependencies.getString(mod) + " and upper");
+					isValid = false;
+				}
 			}
 		}
-		String vscoVersion = versionMap.get("fr.openent.viescolaire").toString();
+		return isValid;
 	}
 
-	private boolean isMajor (String currentVersion, String desireVersion) {
-		return false;
+	private boolean isMajor (String currentVersion, String expectedVersion) {
+		Boolean isMajor = true;
+		String[] splittedCurrentVersion = currentVersion.split("[\\.|-]");
+		String[] splittedExpectedVersion = expectedVersion.split("[\\.|-]");
+		for (int i = 0; i < splittedCurrentVersion.length; i++) {
+			if ("SNAPSHOT".equals(splittedCurrentVersion[i])) { splittedCurrentVersion[i] = "0"; }
+			isMajor = (isMajor && (Integer.parseInt(splittedCurrentVersion[i]) >= Integer.parseInt(splittedExpectedVersion[i])));
+		}
+		return isMajor;
 	}
 
 }
