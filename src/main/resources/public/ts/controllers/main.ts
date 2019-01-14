e9c8b1392734bb416589f42af26312e2d2a2ce3b
remy.seuret <remy.seuret@cgi.com>
2019-01-14 14:12:52 +0100

    ME-72 Can filter on class AND teacher

--- src/main/resources/public/ts/controllers/main.ts
+++ src/main/resources/public/ts/controllers/main.ts
@@ -117,22 +117,22 @@ export let main = ng.controller('EdtController',
          * @returns {Promise<void>}
          */
         $scope.syncCourses = async () => {
-            if ($scope.params.user  && $scope.params.user.length > 0
+       /*     if ($scope.params.user  && $scope.params.user.length > 0
                 && $scope.params.group && $scope.params.group.length > 0) {
 
                 notify.error('Recherche simultannée de profs et de classe ');
-            } else  {
-                if ($scope.isRelative()) {
+          } else  { */
+            if ($scope.isRelative()) {
                     let arrayIds = model.me.classes;
                     let groups = $scope.structure.groups.all;
                     $scope.params.group = groups.filter((item) => arrayIds.indexOf(item.id) > -1);
-                }
+               }
                 $scope.calendarLoader.display();
                 $scope.structure.calendarItems.all = [];
                 await $scope.structure.calendarItems.sync($scope.structure, $scope.params.user, $scope.params.group);
                 $scope.calendarLoader.hide();
                 await   Utils.safeApply($scope);
-            }
+      //      }
         };
 
         if ($scope.isRelative()) {
@@ -303,7 +303,6 @@ export let main = ng.controller('EdtController',
             if(!angular.equals($scope.params.oldGroup, $scope.params.group)){
                 await $scope.syncCourses();
                 initTriggers();
-                $scope.params.user = [];
                 $scope.params.oldGroup = angular.copy($scope.params.group);
 
             }
@@ -313,7 +312,6 @@ export let main = ng.controller('EdtController',
             if(!angular.equals($scope.params.oldUser, $scope.params.user)){
                 await $scope.syncCourses();
                 initTriggers();
-                $scope.params.group = [];
                 $scope.params.oldUser = angular.copy($scope.params.user);
 
             }
