42c94d17e769001a05a6cd1e84b69dcc2b48cc25
remy.seuret <remy.seuret@cgi.com>
2019-04-03 11:25:41 +0200

    ME-126 : fixing 15 minutes on drag and drop

--- src/main/resources/public/ts/model/UtilDragAndDrop.ts
+++ src/main/resources/public/ts/model/UtilDragAndDrop.ts
@@ -83,8 +83,6 @@ export class UtilDragAndDrop {
             let timeslot = model.calendar.timeSlots.all[$(selectedTimeslot).parents('.timeslot').index()];
             let startCourse = moment(model.calendar.firstDay);
             startCourse = startCourse.day(dayOfweek).hour(timeslot.beginning).minute(0).second(0);
-            console.log(15 * (indexHr + 1));
-            console.log(15 * (topPositionnement / 10));
             let endCourse = moment(model.calendar.firstDay);
             endCourse = endCourse.day(dayOfweek).hour(timeslot.end).minute(0).second(0);
             endCourse = endCourse.add(15 * (indexHr + 1), 'minutes').subtract(15 * (topPositionnement / 10), 'minutes');
