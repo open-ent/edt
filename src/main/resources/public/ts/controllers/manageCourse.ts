a69d7bf94a38394b9bec82ca75e7ee487809bf63
c4role <carole.hermouet@cgi.com>
2019-05-20 11:49:46 +0200

    [ME-79] FIX BUG display timeslot indeed free schedule if init only one

--- src/main/resources/public/ts/controllers/manageCourse.ts
+++ src/main/resources/public/ts/controllers/manageCourse.ts
@@ -152,7 +152,10 @@ export let manageCourseCtrl = ng.controller('manageCourseCtrl',
 
             if ($scope.course.timeSlot.start != undefined &&
                 (!moment($scope.courseOccurrenceForm.startTime).isSame(moment(moment($scope.course.startDate).format('YYYY-MM-DD')
-                    + ' ' + $scope.course.timeSlot.start.startHour).toDate()))) {
+                    + ' ' + $scope.course.timeSlot.start.startHour).toDate())) ||
+                (!moment($scope.courseOccurrenceForm.endTime).isSame(moment(moment($scope.course.endDate).format('YYYY-MM-DD')
+                    + ' ' + $scope.course.timeSlot.start.endHour).toDate()))) {
+
                 $scope.display.freeSchedule = true;
             }
 
