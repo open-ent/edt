29c75e491a6fb9b9770c39fe7ea164c3c6225e19
MARTINEAU <flavien.martineau@cgi.com>
2021-01-11 12:15:18 +0100

    chore(): Fix Edt initialisation

--- src/main/java/fr/cgi/edt/Edt.java
+++ src/main/java/fr/cgi/edt/Edt.java
@@ -32,42 +32,5 @@ public class Edt extends BaseServer {
 
         MongoDbConf.getInstance().setCollection(EDT_COLLECTION);
         setDefaultResourceFilter(new ShareAndOwner());
-
-        vertx.setTimer(1000, new Handler<Long>() {
-            @Override
-            public void handle(Long aLong) {
-                if (!validDependencies()) {
-                    LocalMap<String, String> deploymentsIdMap = vertx.sharedData().getLocalMap("deploymentsId");
-                    vertx.undeploy(deploymentsIdMap.get("fr.cgi.edt"));
-                }
-            }
-        });
     }
-
-    private boolean validDependencies () {
-        Boolean isValid = true;
-        if ("prod".equals(config.getString("mode"))) {
-            JsonObject dependencies = config.getJsonObject("dependencies");
-            final LocalMap<Object, Object> versionMap = vertx.sharedData().getLocalMap("versions");
-            for (String mod : dependencies.fieldNames()) {
-                if (!isMajor(versionMap.get(mod).toString(), dependencies.getString(mod))) {
-                    log.error(mod + " minor version. Please upgrade " + mod + " version to " + dependencies.getString(mod) + " and upper");
-                    isValid = false;
-                }
-            }
-        }
-        return isValid;
-    }
-
-    private boolean isMajor (String currentVersion, String expectedVersion) {
-        Boolean isMajor = true;
-        String[] splittedCurrentVersion = currentVersion.split("[\\.|-]");
-        String[] splittedExpectedVersion = expectedVersion.split("[\\.|-]");
-        for (int i = 0; i < splittedCurrentVersion.length; i++) {
-            if ("SNAPSHOT".equals(splittedCurrentVersion[i])) { splittedCurrentVersion[i] = "0"; }
-            isMajor = (isMajor && (Integer.parseInt(splittedCurrentVersion[i]) >= Integer.parseInt(splittedExpectedVersion[i])));
-        }
-        return isMajor;
-    }
-
 }
